ec61b9a9144600648a3af5b62d34fa392b1f3533
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-22 12:11:23 +0000

    commit tree d28460f964884ff65e837366222376b837a4c3f0

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-// Your code here
+// mapToNegativize(sourceArray)
+
+function mapToNegativize(array){
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(-1 * array[i])
+  }
+  return result
+}
+
+// mapToNoChange(sourceArray)
+
+function mapToNoChange(array){
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(array[i])
+  }
+  return result
+}
+
+// mapToDouble(sourceArray)
+
+function mapToDouble(array){
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(2 * array[i])
+  }
+  return result
+}
+
+// mapToSquare(sourceArray)
+
+function mapToSquare(array){
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(array[i] * array[i])
+  }
+  return result
+}
+
+// reduceToTotal(sourceArray, startingPoint)
+
+function reduceToTotal(array, startingPoint=0) {
+  let result = startingPoint
+  for (let i = 0; i < array.length; i++) {
+    result = result + array[i]
+  }
+  return result
+}
+
+
+// reduceToAllTrue(sourceArray)
+
+function reduceToAllTrue(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === false)
+      return false
+  }
+  return true
+}
+
+// reduceToAnyTrue(sourceArray)
+
+function reduceToAnyTrue(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === true)
+      return true
+  }
+  return false
+}
